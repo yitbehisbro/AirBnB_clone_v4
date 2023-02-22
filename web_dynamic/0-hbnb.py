2e8b98c45ca1fd135a651b4da262cbee164b35cd
yitbehisbro <yitbarekw@hu.edu.et>
2023-02-22 11:13:04 +0300

    Do cache without interevention of flask"

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,6 +7,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -21,6 +22,7 @@ def close_db(error):
 @app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
+    cache_id = uuid.uuid4()
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
@@ -37,7 +39,7 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places, cache_id=cache_id)
 
 
 if __name__ == "__main__":
